889161752ee7dcf5033226c1c4239a6cfbcad9f6
Alexander Shepetko <a@shepetko.com>
2017-01-05 21:36:25 +0200

    Release 0.3.2

--- README.md
+++ README.md
@@ -2,18 +2,27 @@
 
 ## Changelog
 
+### 0.3.2 (2017-01-05)
+- Translations fixed.
+- plugin.json updated
+
+
 ### 0.3.1 (2016-12-19)
 plugin.json updated.
+
 
 ### 0.3 (2016-11-28)
 - Support of `content_export` as plugin.
 - plugin.json updated.
 
+
 ### 0.2 (2016-11-25)
 Content export driver ID changed.
 
+
 ### 0.1.1 (2016-11-23)
 Setting IDs changed.
+
 
 ### 0.1 (2016-11-23)
 First release.
--- __init__.py
+++ __init__.py
@@ -1,40 +1,39 @@
 """PytSite VK Package.
 """
+# Public API
 from ._session import Session
+
+# Locally necessary import
+from pytsite import assetman as _assetman, lang as _lang, events as _events, settings as _settings, \
+    permissions as _permissions
+from . import _eh, _settings_form
+from ._content_export import Driver as ContentExportDriver
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+# Resources
+_lang.register_package(__name__, alias='vkontakte')
+_assetman.register_package(__name__, alias='vkontakte')
 
-def __init():
-    from pytsite import assetman, lang, events, settings, permissions
-    from . import _eh, _settings_form
-    from ._content_export import Driver as ContentExportDriver
+# Lang globals
+_lang.register_global('vkontakte_admin_settings_url', lambda language, args: _settings.form_url('vkontakte'))
 
-    # Resources
-    lang.register_package(__name__, alias='vkontakte')
-    assetman.register_package(__name__, alias='vkontakte')
+# Content export driver
+try:
+    from plugins import content_export
 
-    # Lang globals
-    lang.register_global('vkontakte_admin_settings_url', lambda: settings.form_url('vkontakte'))
+    content_export.register_driver(ContentExportDriver())
+except ImportError as e:
+    raise RuntimeError("Required plugin is not found: {}".format(e))
 
-    # Content export driver
-    try:
-        from plugins import content_export
-        content_export.register_driver(ContentExportDriver())
-    except ImportError as e:
-        raise RuntimeError("Required plugin is not found: {}".format(e))
+# Permissions
+_permissions.define_permission('vkontakte.settings.manage', 'vkontakte@manage_vkontakte_settings', 'app')
 
-    # Permissions
-    permissions.define_permission('vkontakte.settings.manage', 'vkontakte@manage_vkontakte_settings', 'app')
+# Settings
+_settings.define('vkontakte', _settings_form.Form, 'vkontakte@vkontakte', 'fa fa-vk', 'vkontakte.settings.manage')
 
-    # Settings
-    settings.define('vkontakte', _settings_form.Form, 'vkontakte@vkontakte', 'fa fa-vk', 'vkontakte.settings.manage')
-
-    # Event handlers
-    events.listen('pytsite.router.dispatch', _eh.router_dispatch)
-    events.listen('pytsite.odm.entity.pre_save.content_export', _eh.odm_entity_pre_save_content_export)
-
-
-__init()
+# Event handlers
+_events.listen('pytsite.router.dispatch', _eh.router_dispatch)
+_events.listen('pytsite.odm.entity.pre_save.content_export', _eh.odm_entity_pre_save_content_export)
